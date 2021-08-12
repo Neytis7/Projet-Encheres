17365edecf151ded005203d486a7edb4d08e5512
Tristan <tristankopec@gmail.com>
2021-08-12 10:46:47 +0200

    [WIP] Début de la connexion à la BDD

---
+++ src/main/java/bll/UsersManager.java
@@ -0,0 +1,33 @@
+package bll;
+
+import bo.User;
+import dal.UserDaoImpl;
+import dal.DALException;
+
+public class UsersManager {
+private UserDaoImpl userDAO = new UserDaoImpl();
+	
+	public UserDaoImpl getUserDAO() {
+		return userDAO;
+	}
+
+	public void setUserDAO(UserDaoImpl userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	public User getUserById(int idUser) throws BLLException {
+		isNotNull(idUser);
+		try {
+			return userDAO.selectUserById(idUser);
+		} catch (DALException dalException) {
+			throw new BLLException(new Exception("La récupération de l'utilisateur à échouée."));
+		}
+	}
+	
+	
+	private void isNotNull(int idUser) throws bll.BLLException {
+		if(idUser == 0) {
+			throw new BLLException(new Exception("L'id user passé en paramètre est égal à 0."));
+		}
+	}
+}
