6a4652f912cfedfdfbd0e5893639c62f0d03ab54
Curtis <curtis.meda2021@campus-eni.fr>
2021-08-19 20:39:06 +0200

    correction merge

--- src/main/java/fr/eni/encheres/controller/signIn.java
+++ src/main/java/fr/eni/encheres/controller/signIn.java
@@ -20,121 +20,124 @@ import fr.eni.encheres.bo.User;
  */
 @WebServlet(description = "Permet à l'utilisateur de se connecter", urlPatterns = {"/sign-in"})
 public class signIn extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public signIn() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public signIn() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/sign-in.jsp");
-		
-		Cookie[] cookies = request.getCookies();
-		
-		if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("login")) {
-                    request.setAttribute("rememberMe", cookie.getValue());
-                    break;
-                }
-            }
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/sign-in.jsp");
+
+    Cookie[] cookies = request.getCookies();
+
+    if (cookies != null) {
+      for (Cookie cookie : cookies) {
+        if (cookie.getName().equals("login")) {
+          request.setAttribute("rememberMe", cookie.getValue());
+          break;
         }
+      }
+    }
 
-		if (rd != null) {
-			rd.forward(request, response);
-		}
-	}
+    if (rd != null) {
+      rd.forward(request, response);
+    }
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-		String btnSignIn = request.getParameter("btnSignIn");
-		List<String> errors = new ArrayList<>();
-		UsersManager usersManager = new UsersManager();
-		String redirectServlet = "WEB-INF/sign-in.jsp";
-		ArrayList<Object> array = new ArrayList<>();
-		User userConnected = null;
-		HttpSession session = request.getSession();
+    String btnSignIn = request.getParameter("btnSignIn");
+    List<String> errors = new ArrayList<>();
+    UsersManager usersManager = new UsersManager();
+    String redirectServlet = "WEB-INF/sign-in.jsp";
+    ArrayList<Object> array = new ArrayList<>();
+    User userConnected = null;
+    HttpSession session = request.getSession();
 
-		if (btnSignIn != null) {
+    if (btnSignIn != null) {
 
-			String loginUser = request.getParameter("loginUser");
-			String passwordUser = request.getParameter("passwordUser");
-			String[] rememberMe = request.getParameterValues("rememberMe");
+      String loginUser = request.getParameter("loginUser");
+      String passwordUser = request.getParameter("passwordUser");
+      String[] rememberMe = request.getParameterValues("rememberMe");
 
-			if (!loginUser.isEmpty() || !passwordUser.isEmpty()) {
-				
-				try {
-					array = usersManager.signInUser(loginUser, passwordUser);
+      if (!loginUser.isEmpty() || !passwordUser.isEmpty()) {
 
-					if (!array.isEmpty()) {
+        try {
+          array = usersManager.signInUser(loginUser, passwordUser);
 
-						userConnected = (User) array.get(0);
-						boolean access = (boolean) array.get(1);
+          if (!array.isEmpty()) {
 
-						if (access) {
+            userConnected = (User) array.get(0);
+            boolean access = (boolean) array.get(1);
 
-							if(rememberMe != null && rememberMe.length > 0) {
-								for(String e : rememberMe) {
-									if("on".equals(e.trim())) {
-										Cookie cookie = new Cookie("login", loginUser);
-										cookie.setMaxAge(60 * 60 * 24 * 30);
-										response.addCookie(cookie);
-										break;
-									}
-								}
-							}else {
-								Cookie[] cookies = request.getCookies();
-								
-								if (cookies != null) {
-						            for (Cookie cookie : cookies) {
-						                if (cookie.getName().equals("login")) {						
-						                    cookie.setMaxAge(0);
-						                    response.addCookie(cookie);
-						                    break;
-						                }
-						            }
-						        }
-							}
-							
-							session.setAttribute("idUserConnected", userConnected.getNo_user());
-							session.setAttribute("nameUserConnected", userConnected.getName());
-							session.setAttribute("firstNameUserConnected", userConnected.getFirst_name());
-							redirectServlet = "./Home";
-						}
-					} else {
-						errors.add("Le login ou le mot de passe est incorrecte !");
-					}
+            if (access) {
 
-				} catch (BLLException exception) {
-					errors.add("Une erreur s'est produite, veuillez réessayer !");
-				}
-			} else {
-				errors.add("Les champs pseudo/email et mot de passe doivent être renseignés.");
-			}
-		}
+              if (rememberMe != null && rememberMe.length > 0) {
+                for (String e : rememberMe) {
+                  if ("on".equals(e.trim())) {
+                    Cookie cookie = new Cookie("login", loginUser);
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    response.addCookie(cookie);
+                    break;
+                  }
+                }
+              } else {
+                Cookie[] cookies = request.getCookies();
 
-		if (errors.size() > 0) {
-			request.setAttribute("errors", errors);
-		}
+                if (cookies != null) {
+                  for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("login")) {
+                      cookie.setMaxAge(0);
+                      response.addCookie(cookie);
+                      break;
+                    }
+                  }
+                }
+              }
 
-		RequestDispatcher rd = request.getRequestDispatcher(redirectServlet);
+              session.setAttribute("idUserConnected", userConnected.getNo_user());
+              session.setAttribute("nameUserConnected", userConnected.getName());
+              session.setAttribute("firstNameUserConnected", userConnected.getFirst_name());
+              redirectServlet = "./Home";
+            } else {
+              redirectServlet = "./sign-up";
+              errors.add("Ce compte est inactif, veuillez en recrée un autre.");
+            }
+          } else {
+            errors.add("Le login ou le mot de passe est incorrecte !");
+          }
 
-		if (rd != null) {
-			rd.forward(request, response);
-		}
-	}
+        } catch (BLLException exception) {
+          errors.add("Une erreur s'est produite, veuillez réessayer !");
+        }
+      } else {
+        errors.add("Les champs pseudo/email et mot de passe doivent être renseignés.");
+      }
+    }
+
+    if (errors.size() > 0) {
+      request.setAttribute("errors", errors);
+    }
+
+    RequestDispatcher rd = request.getRequestDispatcher(redirectServlet);
+
+    if (rd != null) {
+      rd.forward(request, response);
+    }
+  }
 }
